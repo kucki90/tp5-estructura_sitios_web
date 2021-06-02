9a1f6f71a8dbfee4dbf24963d7096d8ef854def7
kucki90 <c.kuckiewicz@hotmail.com>
2021-06-02 18:54:14 -0300

    header

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
+const views = path.join(__dirname, 'views');
 
-app.get('/', (req,res) => res.sendFile('Mi sitio'));
+app.get('/', (req,res) => res.sendFile(path.join(views,'home.html')));
+
+app.use(express.static('public'))
 
 app.listen(port, () => console.log('Servidor corriendo en el puerto ' + port));
